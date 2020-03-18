78944a84222ce4538759f647cb418fb037fbb8ca
Akteruzzaman519 <mdakteruzzaman519@gmail.com>
2020-03-18 06:15:33 +0600

    initial commit

---
+++ Economic/Economic/DatabseOperation/DatabseDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Economic.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Economic.DatabseOperation
+{
+    public class DatabseDbContext:DbContext
+    {
+        public DbSet<Category> Categorys{get; set;}
+        public DbSet<Company> Companys{get; set;}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string conncetionString = "Server=DESKTOP-53FK54F\\SQLEXPRESS; Database=EconomicDB;Integrated Security=true";
+            optionsBuilder.UseSqlServer(conncetionString);
+        }
+    }
+}
---
+++ Economic/Economic/Migrations/20200317145445_initial.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Economic.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categorys",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CategoryName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categorys", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Companys",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CompanyName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Companys", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categorys");
+
+            migrationBuilder.DropTable(
+                name: "Companys");
+        }
+    }
+}
